44323bb18991ed71487461047c87d847d8be6461
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-10-02 13:25:20 +0300

    Dal tamamlandı.

---
+++ WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _categoryService.GetAll();
+            return View(values);
+        }
+    }
+}
