906f58aeab2be44c6e71aee8593852ee6a85a612
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-10-31 17:09:24 +0300

    Blog silme ve güncelleme yapıldı.

--- WebUI/Controllers/BlogController.cs
+++ WebUI/Controllers/BlogController.cs
@@ -61,8 +61,8 @@ namespace WebUI.Controllers
         [HttpPost]
         public IActionResult AddBlog(Blog blog)
         {
-            BlogValidator writerValidator = new BlogValidator();
-            ValidationResult validationResult = writerValidator.Validate(blog);
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult validationResult = blogValidator.Validate(blog);
             if (validationResult.IsValid)
             {
                 blog.BlogStatus = true;
@@ -81,6 +81,51 @@ namespace WebUI.Controllers
             return View();
         }
 
+        public IActionResult DeleteBlog(int id)
+        {
+            var blogValue = _blogService.GetById(id);
+            _blogService.Delete(blogValue);
+            return RedirectToAction("BlogListByWriter", "Blog");
+        }
+
+        [HttpGet]
+        public IActionResult EditBlog(int id)
+        {
+            var value = _blogService.GetById(id);
+
+            List<SelectListItem> categoryValues = (from i in _categoryService.GetAll()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = i.CategoryName,
+                                                       Value = i.CategoryId.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryValues;
+
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EditBlog(Blog blog)
+        {
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult validationResult = blogValidator.Validate(blog);
+            if (validationResult.IsValid)
+            {
+                blog.BlogStatus = true;
+                blog.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                blog.WriterId = 1;
+                _blogService.Update(blog);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
     }
 }
