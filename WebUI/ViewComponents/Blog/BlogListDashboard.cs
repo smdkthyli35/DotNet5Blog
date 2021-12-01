63e1a02f194b71470590973a134c7e72d09ce6a3
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-12-01 20:38:25 +0300

    Dashboard düzenlemeleri yapıldı.

---
+++ WebUI/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        IBlogService _blogService;
+
+        public BlogListDashboard(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ WebUI/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        ICategoryService _categoryService;
+
+        public CategoryListDashboard(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _categoryService.GetAll();
+            return View(result);
+        }
+    }
+}
